21b81979d9968c90f40be2827b6a0cd3defb2c16
Vseotets <metalworldowner@gmail.com>
2021-10-06 12:10:00 +0300

    addd fill stub

--- app.rb
+++ app.rb
@@ -14,10 +14,18 @@ end
 
 rows[0][0] = [basic]
 
+def fill
+
+end
+
 rows[1..deepth].each_with_index do|item, i| 
   (i+2).times {item << []}
   item.first << basic
   item.last << basic
-
+  item.each do |num| 
+    if num.empty?
+      fill
+    end
+  end
 end
 p rows
